bc091b9aad19da96592b1d8e4b48793bd29f6f4e
Sapcodes <sapirsosnovsky@gmail.com>
2016-11-04 14:27:43 -0400

    Added the TransmitTelemetry function

--- Contianer Softare/ContinerSoftwareV1/ContinerSoftwareV1.ino
+++ Contianer Softare/ContinerSoftwareV1/ContinerSoftwareV1.ino
@@ -1,4 +1,4 @@
-void setup() {
+void setup() {das
   // put your setup code here, to run once:
 
 }
@@ -6,4 +6,10 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+String transmitTelemetry (int teamID, unsigned long missionTime, int packetCount, int alt, int tempContainer, float vBatt, int softwareState){
+  
+ String R = String(teamID)+","+String(missionTime)+","+String(packetCount)+","+String(alt)+","+String(tempContainer)+","+String(vBatt)+","+String(softwareState);
+ return (R);
 }
