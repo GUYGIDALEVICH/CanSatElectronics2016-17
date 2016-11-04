6c09512126fe73eaad65d4d00e1aa51d4d45e16c
Sapcodes <sapirsosnovsky@gmail.com>
2016-11-04 14:29:11 -0400

    added comments

--- Contianer Softare/ContinerSoftwareV1/ContinerSoftwareV1.ino
+++ Contianer Softare/ContinerSoftwareV1/ContinerSoftwareV1.ino
@@ -1,4 +1,4 @@
-void setup() {das
+void setup() {
   // put your setup code here, to run once:
 
 }
@@ -8,6 +8,7 @@ void loop() {
 
 }
 
+// Converts the input data to string so it can be exported to the Xbee If questions ask: Sapir Sosnovsky
 String transmitTelemetry (int teamID, unsigned long missionTime, int packetCount, int alt, int tempContainer, float vBatt, int softwareState){
   
  String R = String(teamID)+","+String(missionTime)+","+String(packetCount)+","+String(alt)+","+String(tempContainer)+","+String(vBatt)+","+String(softwareState);
