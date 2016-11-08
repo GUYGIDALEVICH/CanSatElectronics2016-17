7fecd94bb7d0be48b1141c24de4567ff71f27c36
Sapcodes <sapirsosnovsky@gmail.com>
2016-11-07 21:10:25 -0500

    Added a gpsSwitch code
    Ask Sapir Sosnovsky If any questions ;)

--- Contianer Softare/ContinerSoftwareV1/ContinerSoftwareV1.ino
+++ Contianer Softare/ContinerSoftwareV1/ContinerSoftwareV1.ino
@@ -8,9 +8,26 @@ void loop() {
 
 }
 
+
+// potential switch function for GPS by: Sapi rSosnovsky
+Void gpsSwitch(int alt) {
+  
+  if (alt < 100){
+  // some code to tell the gps to start working
+  // some xbee code which will be sent to ground when the alt is close enough to the ground
+  }else{
+  // do nothing if the alt is not close enough to the ground
+  }
+  
+}
+
 // Converts the input data to string so it can be exported to the Xbee If questions ask: Sapir Sosnovsky
 String transmitTelemetry (int teamID, unsigned long missionTime, int packetCount, int alt, int tempContainer, float vBatt, int softwareState){
+// pushes the parameter for the gps switch 
+  gpsSwitch (alt);
   
- String R = String(teamID)+","+String(missionTime)+","+String(packetCount)+","+String(alt)+","+String(tempContainer)+","+String(vBatt)+","+String(softwareState);
- return (R);
+//Converts all input into string which are gonna be sent trough xbee code things
+  String R = String(teamID)+","+String(missionTime)+","+String(packetCount)+","+String(alt)+","+String(tempContainer)+","+String(vBatt)+","+String(softwareState);
+    
+  return (R);
 }
