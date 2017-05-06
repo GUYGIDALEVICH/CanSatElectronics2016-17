6189349d4d580651bcc7931d703d0e87019504ce
Jeff1741 <jeffrey.jev@gmail.com>
2017-05-06 16:10:52 -0400

    Update GPS.ino

--- Container Softare/ContinerSoftwareV2/GPS.ino
+++ Container Softare/ContinerSoftwareV2/GPS.ino
@@ -1,22 +1,17 @@
-//boolean usingInterrupt = false;
-//void useInterrupt(boolean);
-//
-//void setupGPS(){
-//  GPS.begin(9600);
-//  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-//  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
-//  useInterrupt(true);
-//}
-//
-//SIGNAL(TIMER0_COMPA_vect) {
-//  char c = GPS.read();
-//  // if you want to debug, this is a good time to do it!
-//#ifdef UDR0
-//  if (GPSECHO)
-//    if (c) UDR0 = c;  
-//    // writing direct to UDR0 is much much faster than Serial.print 
-//    // but only one character can be written at a time. 
-//#endif
-//}
-//
+void setupGPS()
+{
+  Serial.begin(19200);
+  GPS.begin(9600);
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+}
 
+void callGPS()
+{
+      LocationArray[Latitude] = GPS.latitudeDegrees;
+      LocationArray[Longitude] = GPS.longitudeDegrees;
+      
+//      Serial.print(LocationArray[Latitude]);          Uncomment to see lat/long data in serial monitor (for testing)
+//      Serial.print(", ");                             
+//      Serial.println(GPS.longitudeDegrees,4);    
+}
